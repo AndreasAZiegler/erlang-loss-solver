f8ec34953ba4a8ee9559625cb040bd7978fb0219
Andreas Ziegler <a.ziegler@mt-robot.com>
2020-01-23 18:45:52 +0100

    Some refactoring

--- main.jl
+++ main.jl
@@ -9,29 +9,24 @@ function checkIfStringIsNumber(string)
     return occursin(numbers, string)
 end
 
-function parse_commandline()
+function parseCommandline()
     s = ArgParseSettings()
 
     @add_arg_table s begin
         "--input"
             help = "Select input file"
             arg_type = String
+        "--log"
+            help = "Actevate loggin"
+            arg_type = Bool
+            default = false
     end
 
     return parse_args(s)
 end
 
-function main()
-    parsed_args = parse_commandline()
-    println("Parsed args:")
-    for (arg,val) in parsed_args
-        println("  $arg  =>  $val")
-    end
-
-    input_file_name = parsed_args["input"]
-    println("$input_file_name")
-
-    input_file = readdlm(input_file_name, '\n')
+function parseInput(file_name::String, logging::Bool)
+    input_file = readdlm(file_name, '\n')
     #println("$input_file")
 
     indexes = Dict{String,Int64}()
@@ -44,33 +39,43 @@ function main()
     for (row_index, line) in enumerate(input_file)
         #println("$row_index, $line", typeof(line))
         row = split(line, ",")
-        println("$row")
+        if logging
+          println("$row")
+        end
 
         customer = row[1]
         previous_element = ""
         previous_elements = []
 
         for (col_index, data) in enumerate(row)
-          print("$data, ")
+          if logging
+            print("$data, ")
+          end
           if length(row) == 2 && col_index == 2
               push!(mu, previous_element => parse(Float64, data))
           elseif length(row) == 3 && (length(previous_elements) >= 1 && occursin("LW", previous_elements[1]))
               first_element = previous_elements[1]
-              println("$first_element")
+              if logging
+                println("$first_element")
+              end
               previous_element = data
               push!(previous_elements, data)
               if length(previous_elements) > 2
                   push!(stock_levels, previous_elements[1] => Int64[parse(Int64,previous_elements[2]), parse(Int64, previous_elements[3])])
               end
           elseif checkIfStringIsNumber(data)
-              println("Add entry: [$customer, $previous_element, $data]")
+              if logging 
+                println("Add entry: [$customer, $previous_element, $data]")
+              end
               push!(pairs, [customer, previous_element, parse(Int64, data)])
           else
               if !in(data, nodes)
+                if logging
                   println("Added $data to $index")
-                  push!(indexes, data => index)
-                  push!(nodes, data)
-                  index += 1
+                end
+                push!(indexes, data => index)
+                push!(nodes, data)
+                index += 1
               end
 
               previous_element = data
@@ -80,29 +85,51 @@ function main()
         println("")
     end
 
-    println("$indexes")
-    println("$pairs")
-    println("$nodes")
-    println("$mu")
-    println("$stock_levels")
 
     number_of_nodes = length(nodes)
     connections = zeros(Float64, number_of_nodes, number_of_nodes)
 
     for pair in pairs
+      if logging
         print("pair: $pair: ")
-        pair_1 = pair[1]
-        pair_2 = pair[2]
-        index_1 = indexes[pair_1]
-        index_2 = indexes[pair_2]
-        value = pair[3]
-        println("$index_1, $index_2, $value")
+      end
 
-        connections[index_1, index_2] = value
+      pair_1 = pair[1]
+      pair_2 = pair[2]
+      index_1 = indexes[pair_1]
+      index_2 = indexes[pair_2]
+      value = pair[3]
+
+      connections[index_1, index_2] = value
+    end
+
+    return connections, indexes, pairs, nodes, mu, stock_levels
+end
+
+function main()
+    parsed_args = parseCommandline()
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println("  $arg  =>  $val")
+    end
+
+    input_file_name = parsed_args["input"]
+    println("$input_file_name")
+
+    logging = parsed_args["log"]
+
+    connections, indexes, pairs, nodes, mu, stock_levels = parseInput(input_file_name, logging)
+
+    if logging
+      println("$indexes")
+      println("$pairs")
+      println("$nodes")
+      println("$mu")
+      println("$stock_levels")
     end
 
     for (key, value) in sort(collect(indexes), by=x->x[2])
-        print("$key,  ")
+      print("$key,  ")
     end
     println("")
     for connetions_row in eachrow(connections)
