7487b0d8d4449db7d108872142f85746c03f8fc8
Andreas Ziegler <a.ziegler@mt-robot.com>
2020-01-23 21:28:02 +0100

    Initialize mus

--- main.jl
+++ main.jl
@@ -34,9 +34,28 @@ function main()
     println("")
     for (row_index, connetions_row) in enumerate(eachrow(connections))
       customer = collect(keys(customer_indexes))[row_index]
-      #println("customer: $customer")
       println("$customer $connetions_row")
     end
+
+    T = 2
+    customer_mu = collect(values(mu))
+    storages_mu = Dict{String, Float64}()
+    # Initialize problem
+    for (col_index, connection_col) in enumerate(eachcol(connections))
+      println("col index: $col_index, connection_col: $connection_col")
+      storage_mu = 0
+      for (row_index, cell) in enumerate(connection_col)
+        println("row index: $row_index, cell: $cell")
+
+        if (cell == 1)
+          storage_mu = storage_mu + customer_mu[row_index] * T
+        end
+      end
+      push!(storages_mu, collect(keys(stock_indexes))[col_index] => storage_mu)
+    end
+
+    println("storages mu: $storages_mu")
+
 end
 
 main()
