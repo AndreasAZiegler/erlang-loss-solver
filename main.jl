01005b5bf17eb47c896154beb4ec8871bb3977e3
Andreas Ziegler <062.127@gmail.com>
2020-01-27 22:43:42 +0100

    Added comments

--- main.jl
+++ main.jl
@@ -6,7 +6,10 @@ using .ErlangLossSolverApplication
 using Logging
 
 function main()
+    # Deactivate logging
     Logging.disable_logging(Logging.Warn)
+
+    # Parse args
     parsed_args = ErlangLossSolver.parseCommandline()
 
     input_file_name = parsed_args["input"]
